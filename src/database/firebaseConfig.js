d94fd8b8dc1d72b3aa7c4221c1a3efd2eb52aaa4
Daniele Marques <133058622+D4nnyXM@users.noreply.github.com>
2024-04-26 20:27:09 -0300

    Update firebaseConfig.js
    configuraçao firebase

--- src/database/firebaseConfig.js
+++ src/database/firebaseConfig.js
@@ -1,14 +1,20 @@
+// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
 const firebaseConfig = {
-    apiKey: "AIzaSyAbdsN4VlWRzL0lSEPWb2ibkvknX3zV7rI",
-    authDomain: "ronan-admin-evento.firebaseapp.com",
-    projectId: "ronan-admin-evento",
-    storageBucket: "ronan-admin-evento.appspot.com",
-    messagingSenderId: "345124784036",
-    appId: "1:345124784036:web:aeafbe7f139b649a64e331"
+  apiKey: "AIzaSyAkvIbp9FVJtIUAHWw7d28E3mjRqhY2Ck4",
+  authDomain: "banco-de-dados-adm.firebaseapp.com",
+  projectId: "banco-de-dados-adm",
+  storageBucket: "banco-de-dados-adm.appspot.com",
+  messagingSenderId: "211386821957",
+  appId: "1:211386821957:web:c2d947df30608349e6622c"
 };
 
+// Initialize Firebase
 const app = initializeApp(firebaseConfig);
 export const db = getFirestore(app);
